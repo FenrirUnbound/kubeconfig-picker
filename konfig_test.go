168a3202fee8a5439aed138d463fce325897e736
Darren M <aeneascorrupt@gmail.com>
2019-01-22 22:06:04 -0800

    minor: Create a kubeconfig based on a parent kubeconfig

---
+++ konfig_test.go
@@ -0,0 +1,70 @@
+package konfig_test
+
+import (
+	"io/ioutil"
+	"testing"
+
+	konfig "github.com/fenrirunbound/kubeconfig-picker"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewKonfig(t *testing.T) {
+	testFilepath := "./test_files/valid_file.yaml"
+
+	cfg, err := konfig.NewKonfig(testFilepath)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, cfg)
+}
+
+func TestNewConfigNonexistingConfig(t *testing.T) {
+	fakeFilepath := "./no_file_here.yaml"
+
+	cfg, err := konfig.NewKonfig(fakeFilepath)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, cfg)
+}
+
+func TestListContexts(t *testing.T) {
+	testFilepath := "./test_files/valid_file.yaml"
+	cfg, _ := konfig.NewKonfig(testFilepath)
+
+	contexts := cfg.ListContexts()
+
+	assert.Equal(t, contexts, []string{"myContextName"})
+}
+
+func TestPickContexts(t *testing.T) {
+	testFilepath := "./test_files/bigger_file.yaml"
+	cfg, _ := konfig.NewKonfig(testFilepath)
+
+	slimmerConfig := cfg.SelectContexts([]string{"lastContextName"})
+	assert.NotNil(t, slimmerConfig)
+
+	assert.Equal(t, "randomCluster", slimmerConfig.Contexts["lastContextName"].Cluster)
+	assert.Equal(t, "https://somewhere.com", slimmerConfig.Clusters["randomCluster"].Server)
+	assert.Equal(t, "someUser", slimmerConfig.Contexts["lastContextName"].AuthInfo)
+	assert.Equal(t, []byte("dummyData"), slimmerConfig.AuthInfos["someUser"].ClientKeyData)
+}
+
+func TestPickNonexistingContext(t *testing.T) {
+	testFilepath := "./test_files/bigger_file.yaml"
+	cfg, _ := konfig.NewKonfig(testFilepath)
+
+	slimmerConfig := cfg.SelectContexts([]string{"thisContextCannotExist"})
+	assert.NotNil(t, slimmerConfig)
+	assert.Len(t, slimmerConfig.Contexts, 0)
+}
+
+func TestPickContextsAsYaml(t *testing.T) {
+	testFilepath := "./test_files/bigger_file.yaml"
+	cfg, _ := konfig.NewKonfig(testFilepath)
+
+	slimmerConfig, err := cfg.SelectContextsAsYaml([]string{"lastContextName"})
+	assert.Nil(t, err)
+
+	targetTestContent, _ := ioutil.ReadFile("./test_files/verify_bigger_file.yaml")
+	output := string(slimmerConfig)
+	assert.Equal(t, string(targetTestContent), output)
+}
